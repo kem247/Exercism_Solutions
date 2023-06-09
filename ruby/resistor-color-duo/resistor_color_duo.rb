10a3abf004531fb9b84a6a4e6ea2e894a83f721d
Kemi Thomas <kemothom71@gmail.com>
2023-06-09 19:14:59 -0400

    finished results

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,28 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+=end
+
+class ResistorColorDuo
+    COLORS = {
+        "black" => 0,
+        "brown" => 1,
+        "red" => 2,
+        "orange" => 3,
+        "yellow" => 4,
+        "green" => 5,
+        "blue" => 6,
+        "violet" => 7,
+        "grey" => 8,
+        "white" => 9
+
+    }
+    def self.value(array)
+    _first = COLORS[array[0]]
+    _second = COLORS[array[1]]
+    return (_first.to_s+_second.to_s).to_i
+    end 
+end
